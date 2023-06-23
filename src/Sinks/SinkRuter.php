8d94e8fe6da5c45f971096593e6a22aba06e95f0
Kåre Slettnes <kaare.slettnes@tffk.no>
2023-06-23 14:57:34 +0200

    Implement abstract function

--- src/Sinks/SinkRuter.php
+++ src/Sinks/SinkRuter.php
@@ -7,4 +7,14 @@ use TromsFylkestrafikk\RagnarokSink\Sinks\SinkBase;
 class SinkRuter extends SinkBase
 {
     public $name = "Ruter";
+
+    /**
+     * Import one chunk from sink.
+     *
+     * @return bool
+     */
+    public function import(): bool
+    {
+        return true;
+    }
 }
