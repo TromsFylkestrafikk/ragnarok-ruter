7c4790a8c227b55a4da672bae662c5a12170d341
Kåre Slettnes <kaare.slettnes@tffk.no>
2023-06-30 10:33:28 +0200

    Keep up with api

--- src/Sinks/SinkRuter.php
+++ src/Sinks/SinkRuter.php
@@ -2,11 +2,29 @@
 
 namespace TromsFylkestrafikk\RagnarokRuter\Sinks;
 
+use Illuminate\Support\Carbon;
 use TromsFylkestrafikk\RagnarokSink\Sinks\SinkBase;
 
 class SinkRuter extends SinkBase
 {
-    public $name = "Ruter";
+    public $id = "ruter";
+    public $title = "Ruter";
+
+    /**
+     * @inheritdoc
+     */
+    public function getFromDate(): Carbon
+    {
+        return new Carbon('2020-06-01');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getToDate(): Carbon
+    {
+        return today()->subDay();
+    }
 
     /**
      * Import one chunk from sink.
